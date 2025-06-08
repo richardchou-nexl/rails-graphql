e1f88273cce35c140d0d0d2ec076f1ce97e1eaba
Richard Chou <richard@nexl.cloud>
2025-06-08 10:10:54 +1000

    added user query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,30 +2,20 @@
 
 module Types
   class QueryType < Types::BaseObject
-    field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
-      argument :id, ID, required: true, description: "ID of the object."
+    # get all users
+    field :users, [ Types::UserType ], null: false
+
+    def users
+      User.all
     end
 
-    def node(id:)
-      context.schema.object_from_id(id, context)
+    # get a specific user by ID
+    field :user, Types::UserType, null: true do
+      argument :id, ID, required: true
     end
 
-    field :nodes, [ Types::NodeType, null: true ], null: true, description: "Fetches a list of objects given a list of IDs." do
-      argument :ids, [ ID ], required: true, description: "IDs of the objects."
-    end
-
-    def nodes(ids:)
-      ids.map { |id| context.schema.object_from_id(id, context) }
-    end
-
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def user(id:)
+      User.find(id)
     end
   end
 end
